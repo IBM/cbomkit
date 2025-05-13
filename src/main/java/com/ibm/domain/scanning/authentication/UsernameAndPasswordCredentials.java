691c4fba0f7b6e52c17c64c5f5d089c58d039981
Nicklas Körtge <nicklas.koertge1@ibm.com>
2025-05-13 13:59:43 +0200

    replace IBM with PQCA in licence header
    Signed-off-by: Nicklas Körtge <nicklas.koertge1@ibm.com>

--- src/main/java/com/ibm/domain/scanning/authentication/UsernameAndPasswordCredentials.java
+++ src/main/java/com/ibm/domain/scanning/authentication/UsernameAndPasswordCredentials.java
@@ -1,6 +1,6 @@
 /*
  * CBOMkit
- * Copyright (C) 2024 IBM
+ * Copyright (C) 2024 PQCA
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
